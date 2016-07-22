c2d3489039715999385022cf21522af126afbbb2
Rushabh <rs@rushabhs.com>
2016-07-22 11:39:09 -0700

    p02_hashing.rb

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    self.join.split("").map{|el| el.ord}.join.to_i.hash
   end
 end
 
 class String
   def hash
+    self.split("").hash
   end
 end
 
@@ -16,6 +18,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort_by {|key, value| key}.flatten.hash
   end
 end
